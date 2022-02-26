5b7aa2329cb156e5e0d28f065f314a81eaa3dd63
Praveen Kumar <popinzs241199@gmail.com>
2022-02-26 13:39:21 +0530

    first commit

---
+++ src/routes/route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const UserModel= require("../models/userModel.js")
+const UserController= require("../controllers/userController")
+
+
+
+router.post("/createUser", UserController.createUser  )
+
+router.get("/getUsersData", UserController.getUsersData)
+
+module.exports = router;
